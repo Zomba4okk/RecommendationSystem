054e74efffc043da125b784d8c1aea694257c299
Zomba4okk <antickz28@gmail.com>
2020-03-22 21:35:09 +0300

    Add custom params to product model

--- app/product/models.py
+++ app/product/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Float, String, Integer, ForeignKey
+from sqlalchemy import Column, Float, String, Integer, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
 from app.base import Base
@@ -7,6 +7,9 @@ from app.base import Base
 class Product(Base):
     name = Column(String(50))
     price = Column(Float)
+    param_1 = Column(Integer)
+    param_2 = Column(Integer)
+    param_3 = Column(Boolean)
     avg_rating = Column(Float)
     ratings = relationship('Rating', back_populates='product')
 
