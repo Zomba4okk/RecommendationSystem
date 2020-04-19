955ef7c19a586d9b1730218d68c6aa53dc4ffd6b
Zomba4okk <antickz28@gmail.com>
2020-04-19 05:36:49 +0300

    Fix bug

--- prediction_system/naive_predict.py
+++ prediction_system/naive_predict.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def naive_predict_user(rating, users_count, products_count, distance_matrix):
+def naive_predict_user(rating, users_count, products_count, distance_matrix, train_data_matrix):
     top_similar_ratings = np.zeros((users_count, rating, products_count))
 
     for i in range(users_count):
         top_sim_users = distance_matrix[i].argsort()[1:rating + 1]
 
-        top_similar_ratings[i] = distance_matrix[top_sim_users]
+        top_similar_ratings[i] = train_data_matrix[top_sim_users]
 
     pred = np.zeros((users_count, products_count))
     for i in range(users_count):
@@ -16,12 +16,12 @@ def naive_predict_user(rating, users_count, products_count, distance_matrix):
     return pred
 
 
-def naive_predict_product(rating, users_count, products_count, distance_matrix):
+def naive_predict_product(rating, users_count, products_count, distance_matrix, train_data_matrix):
     top_similar_ratings = np.zeros((products_count, rating, users_count))
 
     for i in range(products_count):
-        top_sim_movies = distance_matrix[i].argsort()[1:rating + 1]
-        top_similar_ratings[i] = distance_matrix.T[top_sim_movies]
+        top_sim_products = distance_matrix[i].argsort()[1:rating + 1]
+        top_similar_ratings[i] = train_data_matrix.T[top_sim_products]
 
     pred = np.zeros((products_count, users_count))
     for i in range(products_count):
